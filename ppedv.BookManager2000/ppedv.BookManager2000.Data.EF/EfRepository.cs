409a7c072378339bef5dbebefda09f32fc045fdd
André <andrerrr@outlook.de>
2019-03-20 15:19:02 +0100

    Fertig mit Repository

---
+++ ppedv.BookManager2000/ppedv.BookManager2000.Data.EF/EfRepository.cs
@@ -0,0 +1,45 @@
+﻿using ppedv.BookManager2000.Model;
+using ppedv.BookManager2000.Model.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.BookManager2000.Data.EF
+{
+    public class EfRepository : IRepository
+    {
+        private EfContext context;
+
+        public void Add<T>(T entity) where T : Entity
+        {
+            context.Set<T>().Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : Entity
+        {
+            context.Set<T>().Remove(entity);
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : Entity
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public T GetById<T>(int id) where T : Entity
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public void SaveChanges()
+        {
+            context.SaveChanges();
+        }
+
+        public void Update<T>(T entity) where T : Entity
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ppedv.BookManager2000/ppedv.BookManager2000.Logic/Core.cs
@@ -0,0 +1,28 @@
+﻿using ppedv.BookManager2000.Model;
+using ppedv.BookManager2000.Model.Contracts;
+using System;
+using System.Linq;
+
+namespace ppedv.BookManager2000.Logic
+{
+    public class Core
+    {
+        public IRepository Repository { get; }
+
+
+        public Book GetBookWithLongestAutorenNamen()
+        {
+            return Repository.GetAll<Book>().OrderBy(x => x.Autoren.Sum(y => y.Name.Length)).FirstOrDefault();
+        }
+
+
+        public Core(IRepository repo)
+        {
+            this.Repository = repo;
+        }
+
+        public Core() : this(new Data.EF.EfRepository())
+        { }
+
+    }
+}
